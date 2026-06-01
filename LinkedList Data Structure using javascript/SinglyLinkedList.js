308a0fdf6721831830de9f779d3a766ec54d17c0
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-01 09:21:29 +0530

    Singly linked list  using javascript 1.2

--- LinkedList Data Structure using javascript/SinglyLinkedList.js
+++ LinkedList Data Structure using javascript/SinglyLinkedList.js
@@ -5,3 +5,44 @@ class Node {
     }
 }
 
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+        if(!this.head) {
+            this.head = newNode;
+        }else {
+            let current = this.head;
+            while(current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.size++;
+
+    }
+
+    prepend(data) {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++;
+    }
+
+    insertAt(data,index) {
+        if(index <0 || index >this.size) {
+            console.error('Invalid index..')
+            return false;
+        }
+
+        if(index === 0) {
+            this.prepend(data);
+            return true;
+        }
+    }
+
+}
